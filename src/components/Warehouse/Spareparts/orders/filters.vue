f4ca383ba858ee8596610117261d0200eb710194
TheWolek <pawelm916@wp.pl>
2022-04-10 13:33:57 +0200

    add close image on filter chips

--- src/components/Warehouse/Spareparts/orders/filters.vue
+++ src/components/Warehouse/Spareparts/orders/filters.vue
@@ -1,31 +1,40 @@
 <script>
-import { mapState } from 'vuex'
-import store from '../../../../store'
+import { mapState } from "vuex";
+import store from "../../../../store";
 export default {
-    computed: {
-        ...mapState({
-            appliedFilters: state => state.sparepartsOrders.appliedFilters
-        })
+  computed: {
+    ...mapState({
+      appliedFilters: (state) => state.sparepartsOrders.appliedFilters,
+    }),
+  },
+  methods: {
+    deleteFilter(filter) {
+      store.dispatch("sparepartsOrders/deleteFilter", filter);
     },
-    methods: {
-        deleteFilter(filter) {
-            store.dispatch("sparepartsOrders/deleteFilter", filter)
-        }
-    }
-}
+  },
+};
 </script>
 <template>
-    <div>
-        <div class="filterWrap" :class="{active: this.appliedFilters.active}">
-            <div v-for="(val, key) in appliedFilters.names" :key="key" class="filterChip" @click="()=>{deleteFilter(key)}">
-                {{val[0]}}
-                <div class="close"></div>
-            </div>
-        </div>
+  <div>
+    <div class="filterWrap" :class="{ active: this.appliedFilters.active }">
+      <div
+        v-for="(val, key) in appliedFilters.names"
+        :key="key"
+        class="filterChip"
+        @click="
+          () => {
+            deleteFilter(key);
+          }
+        "
+      >
+        <p>{{ val[0] }}</p>
+        <img src="@/assets/cancel.svg" />
+      </div>
     </div>
+  </div>
 </template>
 <style scoped>
-    .filterWrap {
-        height: 23px;
-    }
+.filterWrap {
+  height: 23px;
+}
 </style>
