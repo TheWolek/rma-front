9bd1a581ef2807f49e2418c58f876ef481e1979f
TheWolek <pawelm916@wp.pl>
2022-06-18 12:46:45 +0200

    fetchByFilters onRefresh and refresh button spin animation when loading

--- src/components/Warehouse/Spareparts/actions.vue
+++ src/components/Warehouse/Spareparts/actions.vue
@@ -5,11 +5,15 @@ export default {
   computed: {
     ...mapState({
       parts: (state) => state.spareparts.parts,
+      loading: (state) => state.spareparts.refreshingTable,
     }),
   },
   methods: {
     toggleFindModal() {
       store.commit("spareparts/toggleFindModal");
+    },
+    onRefresh() {
+      store.dispatch("spareparts/fetchPartsByFilters");
     },
   },
 };
@@ -24,7 +28,7 @@ export default {
       <img src="@/assets/search.svg" class="searchImg" /> Szukaj
     </div>
     <div class="actionBtn refreshBtn" id="btn5" @click="onRefresh">
-      <img src="@/assets/refresh.svg" :class="{ active: false }" />
+      <img src="@/assets/refresh.svg" :class="{ active: this.loading }" />
     </div>
   </div>
 </template>
